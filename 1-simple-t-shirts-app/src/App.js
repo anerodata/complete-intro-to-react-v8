642173a4eea0ad5a15ab7a1b8b90ba8e91a432a2
anerodata <antonio92.ahr@gmail.com>
2023-04-08 15:14:17 +0200

    Create first simple app

---
+++ 1-simple-t-shirts-app/src/App.js
@@ -0,0 +1,32 @@
+const tShirt = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, `${props.label} - ${props.color} - ${props.size}`),
+  ])
+}
+const App = () => {
+  return React.createElement(
+    'div',
+    {}, 
+    [
+      React.createElement('h1', {}, 'Adopt me!'),
+      React.createElement(tShirt, {
+        label: "Nike",
+        color: "Grey",
+        size: "Large"
+      }),
+      React.createElement(tShirt, {
+        label: "Tribord",
+        color: "Black",
+        size: "Extra large"
+      }),
+      React.createElement(tShirt, {
+        label: "Reebok",
+        color: "Red",
+        size: "Medium"
+      })
+    ]
+  )
+}
+const rootContainer = document.querySelector('#root')
+const root = ReactDOM.createRoot(rootContainer)
+root.render(React.createElement(App))
