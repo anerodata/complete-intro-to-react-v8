4dac71932b0136b71c3d586d70987356c95ef75a
anerodata <antonio92.ahr@gmail.com>
2023-04-09 12:38:26 +0200

    Add some comments

--- 1-simple-t-shirts-app/src/SearchParams.jsx
+++ 1-simple-t-shirts-app/src/SearchParams.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react"
 const SearchParams = () => {
   const [location, setLocation] = useState("Houston, TX")
+  // Same as:
+  // const locationHook = useState("")
+  // const location = locationHook[0]
+  // const setLocation = locationHook[1]
   return (
     <div className="search-params">
       <form>
