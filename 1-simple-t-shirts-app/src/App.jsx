2ecce67fe940a08da5a0aa5ba23378e894cb4740
anerodata <antonio92.ahr@gmail.com>
2023-04-12 23:09:07 +0200

    Use react-query

--- 1-simple-t-shirts-app/src/App.jsx
+++ 1-simple-t-shirts-app/src/App.jsx
@@ -1,17 +1,30 @@
 import { createRoot } from "react-dom/client"
 import SearchParams from "./SearchParams"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import Details from "./Details"
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity
+    }
+  }
+})
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <h1>Awesome t-shirts</h1>
-        <Routes>
-          <Route path="/details/:id" element={<Details/>}/>
-          <Route path="/" element={<SearchParams/>}/>
-        </Routes>
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt me!</Link>
+        </header>
+        <div>
+          <Routes>
+            <Route path="/details/:id" element={<Details/>}/>
+            <Route path="/" element={<SearchParams/>}/>
+          </Routes>
+        </div>
+      </QueryClientProvider>
     </BrowserRouter>
   )
 }
