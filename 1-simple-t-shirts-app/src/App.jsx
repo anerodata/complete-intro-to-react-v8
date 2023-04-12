dbd88779bbc011aaedcdabe142c3365e8c8635ff
anerodata <antonio92.ahr@gmail.com>
2023-04-12 20:48:26 +0200

    React router

--- 1-simple-t-shirts-app/src/App.jsx
+++ 1-simple-t-shirts-app/src/App.jsx
@@ -1,11 +1,19 @@
 import { createRoot } from "react-dom/client"
 import SearchParams from "./SearchParams"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Details from "./Details"
 const App = () => {
   return (
-    <div>
-      <h1>Awesome t-shirts</h1>
-      <SearchParams/>
-  </div>
+    <BrowserRouter>
+      <div>
+        <h1>Awesome t-shirts</h1>
+        <SearchParams/>
+        <Routes>
+          <Route path="/details/:id" element={<Details/>}/>
+          <Route path="/" element={<SearchParams/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 const rootContainer = document.querySelector('#root')
