eb1861c50604d0a9c9d5f961b6b62ddbaa232fef
Antonio Hernandez <ahernandez@nalandaglobal.com>
2023-06-20 10:22:20 +0200

    Crear funcionalidad de reemplaco de caracteres utf por unicode

--- unicode-app/src/App.jsx
+++ unicode-app/src/App.jsx
@@ -4,10 +4,18 @@ import ConverterSection from './ConverterSection.jsx'
 
 const App = () => {
   const [ textModifiedHook, setTextModifiedHook ] = useState('')
+  const replaceUTFWithUnicode = (str) => {
+    const regex = /[^\u{0000}-\u{007F}]/gu
+    return str.replace(regex, (match) => {
+      const codePoint = match.codePointAt(0).toString(16)
+      const amountOfZeros = 4 - codePoint.length
+      return '\\u' + '0'.repeat(amountOfZeros < 0 ? 0 : amountOfZeros) + codePoint
+    })
+  }
   return (
     <div>
       <ConverterSection onTextToReplaceChange={(value) => {
-        setTextModifiedHook(value+'sss')
+        setTextModifiedHook(replaceUTFWithUnicode(value))
       }} valueModified={textModifiedHook} />
     </div>
   )
