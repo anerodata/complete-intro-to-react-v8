3d0f71c7e29d955d66938848754dc5f238035b88
Antonio Hernandez <ahernandez@nalandaglobal.com>
2023-06-20 07:57:05 +0200

    Eliminar valor no usado, identar

--- unicode-app/src/App.jsx
+++ unicode-app/src/App.jsx
@@ -5,11 +5,11 @@ import ConverterSection from './ConverterSection.jsx'
 const App = () => {
   const [ textToConvertHook, setTextToConvertHook ] = useState('')
   return (
-  <div>
-    <ConverterSection readOnly={false} onTextToReplaceChange={(value) => {
-      setTextToConvertHook(value)
-    }} value={textToConvertHook} />
-</div>
+    <div>
+      <ConverterSection onTextToReplaceChange={(value) => {
+        setTextToConvertHook(value)
+      }} value={textToConvertHook} />
+    </div>
   )
 }
 const rootContainer = document.querySelector('#root')
--- unicode-app/src/ConverterSection.jsx
+++ unicode-app/src/ConverterSection.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 const ConverterSection = (props) => {
-const [textToReplaceHook, setTextToReplaceHook ] = useState(props.value)
+const [_, setTextToReplaceHook ] = useState(props.value)
   return (
     <section>
-      <textarea readOnly={props.readOnly} value={props.value} cols="30" rows="10" onChange={(e) => {
+      <textarea value={props.value} cols="30" rows="10" onChange={(e) => {
         const newText = e.target.value
         setTextToReplaceHook(newText)
         props.onTextToReplaceChange(newText)
